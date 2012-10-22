43a799e5a599d2b030cbc6b06ab98f45e1311354
nandorrb <nando.rrb.00@gmail.com>
2012-10-22 16:23:54 -0500

    Readme de la clase

--- PaginaWEB/src/com/example/paginaweb/Surfer.java
+++ PaginaWEB/src/com/example/paginaweb/Surfer.java
@@ -2,6 +2,12 @@ package com.example.paginaweb;
 
 import android.app.Activity;
 import android.os.Bundle;
+/* README
+surfer es la clase que esta destinada a navegar entre producto y producto, caracteristicas:
+-producto de menor precio
+-producto de mayor calidad
+-producto equivalente
+*/
 
 public class Surfer extends Activity{
 
