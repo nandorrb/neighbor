10c04995366e20b0fd681aa67b68bdc484948f83
Fernando <nando.rrb.00@gmail.com>
2012-10-23 12:37:48 -0500

    comentarios de ayuda

--- PaginaWEB/src/com/example/paginaweb/ChangePlace.java
+++ PaginaWEB/src/com/example/paginaweb/ChangePlace.java
@@ -13,12 +13,12 @@ public class ChangePlace extends Activity  {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-
+//se llama al barcode scanner
 		Intent intent = new Intent("com.google.zxing.client.android.SCAN");
 		intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
 		startActivityForResult(intent, 0);
 	}
-
+// se recibe la respuesta y se trabaja
 	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
 		if (requestCode == 0) {
 			if (resultCode == RESULT_OK) {
